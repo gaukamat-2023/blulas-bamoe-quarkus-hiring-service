4e7eb82c853c640ea46a683c30dde42e65b05c13
gaukamat <gaukamat@in.ibm.com>
2025-12-07 12:00:47 +0530

    First Commit: Hiring Service - BAMOE Quarkus from Barry Lulas

---
+++ src/main/java/org/acme/CandidateData.java
@@ -0,0 +1,69 @@
+package org.acme;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+
+public class CandidateData {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer experience;
+    private List<String> skills;
+
+    public CandidateData() {
+    }
+
+    public CandidateData(String name, String lastName, String email, Integer experience, List<String> skills) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.experience = experience;
+        this.skills = skills;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    @JsonIgnore
+    public String getFullName() {
+        return name + " " + lastName;
+    }
+}
---
+++ src/main/java/org/acme/Offer.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+public class Offer {
+
+    private String category;
+    private Integer salary;
+
+    public Offer() {
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
